7c35a2bba8d055e3af0dbdb1d2b9373f10b65801
jmonto55 <jmonto55@gmail.com>
2023-01-11 12:32:14 -0500

    Add active effect on nav bar items

--- index.js
+++ index.js
@@ -136,6 +136,9 @@ window.onload = () => {
 
 function listHandler(event) {
   event.preventDefault();
+  listLink.style.color = 'blue'
+  addBookLink.style.color = 'black'
+  contactLink.style.color = 'black'
   booksSection.style.display = 'block';
   addBookSection.style.display = 'none';
   contactSection.style.display = 'none';
@@ -143,6 +146,9 @@ function listHandler(event) {
 
 function addBookHandler(event) {
   event.preventDefault();
+  listLink.style.color = 'black'
+  addBookLink.style.color = 'blue'
+  contactLink.style.color = 'black'
   booksSection.style.display = 'none';
   addBookSection.style.display = 'flex';
   contactSection.style.display = 'none';
@@ -150,6 +156,9 @@ function addBookHandler(event) {
 
 function contactHandler(event) {
   event.preventDefault();
+  listLink.style.color = 'black'
+  addBookLink.style.color = 'black'
+  contactLink.style.color = 'blue'
   booksSection.style.display = 'none';
   addBookSection.style.display = 'none';
   contactSection.style.display = 'block';
